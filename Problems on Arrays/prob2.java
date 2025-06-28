1e79e083cf2db5e2b7a5340412406ca811682a49
AIGautam <kumargautam.gk0039@gmail.com>
2025-06-28 09:30:26 +0530

    add findSmallest in prob2

--- Problems on Arrays/prob2.java
+++ Problems on Arrays/prob2.java
@@ -1,4 +1,4 @@
-// Find the Largest element in an array
+// Find the Largest & smallest element in an array
 
 public class prob2 {
     public static int findLargest(int[] arr) {
@@ -10,11 +10,22 @@ public class prob2 {
         }
         return largest;
     }
+    public static int findSmallest(int[] arr){
+        int smallest = arr[0];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] < smallest){
+                smallest = arr[i];
+            }
+        }
+        return smallest;
+    }
 
     public static void main(String[] args) {
         int[] arr = {2, 5, 4, 3, 1, 0};
         int result = findLargest(arr);
+        int result_1 = findSmallest(arr);
         System.out.println("Largest element: " + result);
+        System.out.println("Smallest element: " +result_1);
     }
 }
 
