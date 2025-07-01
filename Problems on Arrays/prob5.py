4665758793d1823f17d171353e5cd98487f1ae29
AIGautam <kumargautam.gk0039@gmail.com>
2025-07-01 22:17:00 +0530

    prob6

--- Problems on Arrays/prob5.py
+++ Problems on Arrays/prob5.py
@@ -5,3 +5,26 @@ arr = [3,4,3,5,4,3]
 freq = Counter(arr)
 for key, value in freq.items():
     print(key, value)
+    
+    
+    
+    # In java code
+    
+#     import java.util.*;
+
+# public class FrequencyCount {
+#     public static void main(String[] args) {
+#         int[] arr = {10, 5, 10, 15, 10, 5};
+        
+#         HashMap<Integer, Integer> map = new HashMap<>();
+        
+#         for (int num : arr) {
+#             // put count or increase it if already exists
+#             map.put(num, map.getOrDefault(num, 0) + 1);
+#         }
+        
+#         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+#             System.out.println(entry.getKey() + " " + entry.getValue());
+#         }
+#     }
+# }
---
+++ Problems on Arrays/prob6.py
@@ -0,0 +1,28 @@
+def rearrange_inc_dec(arr):
+    arr.sort()
+    n = len(arr)
+    mid = n//2
+    result = arr[:mid] + arr[mid:][::-1]
+    return result
+arr = [2,3,1,4,9,6]
+print(rearrange_inc_dec(arr))
+
+
+
+
+# import java.util.Arrays;
+# public class Main {
+
+#   public static void main(String args[]) {
+
+#     int arr[] = {8,7,1,6,5,9};
+#     int n = arr.length;
+#     Arrays.sort(arr);
+#     for (int i = 0; i < n / 2; i++) {
+#       System.out.print(arr[i] + " ");
+#     }
+#     for (int i = n - 1; i >= n / 2; i--) {
+#       System.out.print(arr[i] + " ");
+#     }
+# }
+# }
