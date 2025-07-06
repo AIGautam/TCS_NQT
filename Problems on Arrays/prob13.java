a40d57af16d13910b63accd3222bf9491409e34c
AIGautam <kumargautam.gk0039@gmail.com>
2025-07-06 18:59:37 +0530

    ascii value

---
+++ Problems on Arrays/prob13.java
@@ -0,0 +1,17 @@
+// Find the ASCII value of a character
+
+import java.util.Scanner;
+
+public class prob13 {
+    static void  getAscii(char ch){
+        int ascii =  ch;
+        System.out.println(ascii);
+    }
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        char ch = sc.next().charAt(0); 
+        getAscii(ch);
+        sc.close();
+
+    } 
+}
