37b30128f06138b54d122d610565e9659fe59923
AIGautam <kumargautam.gk0039@gmail.com>
2025-07-06 18:04:41 +0530

    Check if the given String is Palindrome or not

---
+++ Problems on Arrays/prob11.java
@@ -0,0 +1,25 @@
+// Check if the given String is Palindrome or not
+
+
+
+import java.util.Scanner;
+
+public class prob11 {
+    static boolean isPalindrome(String str){
+        str = str.toLowerCase().replaceAll("\\s+", "");
+        String reversed  = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String str  = sc.nextLine();
+        if(isPalindrome(str)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("NOT Palindrome");
+        }
+        sc.close();
+
+    }
+}
