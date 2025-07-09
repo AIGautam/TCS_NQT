585f9ef6c66e41b7ec62f2ae98d252e5d5d23ff0
AIGautam <kumargautam.gk0039@gmail.com>
2025-07-10 02:04:52 +0530

    add prob4 in string

--- Problems on Strings/prob4.java
+++ Problems on Strings/prob4.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class prob4 {
+    public static String RemoveVowels(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+                str = str.substring(0, i) + str.substring(i + 1);
+                i--;
+            }
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = RemoveVowels(str);
+        System.out.println(result);
+        sc.close();
     }
 }
