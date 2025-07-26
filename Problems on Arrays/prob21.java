cb4762559fbad5c9e495f6b6961d3e283b488a6e
AIGautam <kumargautam.gk0039@gmail.com>
2025-07-26 13:07:23 +0530

    arr20,21

---
+++ Problems on Arrays/prob21.java
@@ -0,0 +1,22 @@
+// Maximum Subarray
+
+public class prob21 {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0];  
+        int currMax = nums[0];   
+
+        for (int i = 1; i < nums.length; i++) {
+            currMax = Math.max(nums[i], currMax + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currMax);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int result = maxSubArray(arr);
+        System.out.println("Maximum Subarray Sum: " + result);  // Output: 6
+    }
+}
