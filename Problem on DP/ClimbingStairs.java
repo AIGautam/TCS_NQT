61ec26280d7b3b3c56f6fb27376a860f21ef3e35
AIGautam <kumargautam.gk0039@gmail.com>
2025-08-31 02:44:44 +0530

    dp, queue

--- Problem on DP/ClimbingStairs.java
+++ Problem on DP/ClimbingStairs.java
@@ -11,11 +11,27 @@ public class ClimbingStairs {
         ways[n] = ClimbWays(n-1, ways) + ClimbWays(n-2, ways);
         return ways[n];
     }
+
+
+    //tabulation loop
+    public static int cntWaysTab(int n){
+        int dp[] = new int [n+1];
+        dp[0] = 1;
+        for(int i = 1; i<=n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1] + 0;
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int ways[] = new int[n+1];
         Arrays.fill(ways, -1);
         System.out.println(ClimbWays(n, ways));
+        System.out.println(cntWaysTab(n));
 
     }
 }
